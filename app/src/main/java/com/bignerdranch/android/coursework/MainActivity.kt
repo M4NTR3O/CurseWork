4393e1b6c1d247334c5ea85e8e35ec6e5f3427fb
emil_ <emil.kushner.stud@mail.ru>
2024-05-11 00:25:57 +0700

    Навигация между фрагментами

--- app/src/main/java/com/bignerdranch/android/coursework/MainActivity.kt
+++ app/src/main/java/com/bignerdranch/android/coursework/MainActivity.kt
@@ -1,11 +1,49 @@
 package com.bignerdranch.android.coursework
 
+import android.app.FragmentManager
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.fragment.app.Fragment
+import com.bignerdranch.android.coursework.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        replaceFragment(MainFragment())
+
+        binding.bottomNavigationView.setOnItemSelectedListener{
+            item ->
+            when(item.itemId) {
+                R.id.favourites -> {
+                    replaceFragment(FavouritesFragment())
+                    true
+                }
+
+                R.id.main -> {
+                    replaceFragment(MainFragment())
+                    true
+                }
+
+                R.id.search -> {
+                    replaceFragment(SearchFragment())
+                    true
+                }
+
+                else -> {
+                    true
+                }
+            }
+        }
+    }
+
+    private fun replaceFragment(fragment: Fragment){
+        var fragmentManager = supportFragmentManager
+        var fragmentTransaction = fragmentManager.beginTransaction()
+        fragmentTransaction.replace(R.id.frameLayout, fragment)
+        fragmentTransaction.commit()
     }
 }
